9be88774da5e0683ef72619212a4b2c4ffa2835f
Andre Mayer <andremayerusa@gmail.com>
2023-02-26 02:10:27 -0300

    add view  and form for sharing posts through email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'andremayerproject@gmail.com'
+EMAIL_HOST_PASSWORD = "hkaqudrvjmyupvlo"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
